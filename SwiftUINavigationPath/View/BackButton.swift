50ac451afca4efe995727a433f5a816f4158a305
Angelos Staboulis <angelosstaboulis@gmail.com>
2024-06-07 13:23:28 +0300

    Commit refactor code(made change in custom back button view)

---
+++ SwiftUINavigationPath/View/BackButton.swift
@@ -0,0 +1,18 @@
+//
+//  BackButton.swift
+//  SwiftUINavigationPath
+//
+//  Created by Angelos Staboulis on 7/6/24.
+//
+
+import SwiftUI
+
+struct BackButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BackButton()
+}
